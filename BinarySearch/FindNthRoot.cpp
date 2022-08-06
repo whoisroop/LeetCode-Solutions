5aff4dc9dad76728fdc527d934d4da14f31c6432
RavenClaaaw <81151656+RavenClaaaw@users.noreply.github.com>
2022-08-06 22:45:01 +0530

    Update FindNthRoot.cpp

--- BinarySearch/FindNthRoot.cpp
+++ BinarySearch/FindNthRoot.cpp
@@ -4,15 +4,24 @@ using namespace std;
 
 class Solution{
 public:
+    double power(double x, int n){ //x*x*x*... n times
+        if(n==0)    return 1;
+
+        double t = power(x, n/2);
+        if(n%2==0)  return t*t;
+        else    return x*t*t;
+    }
+
     double findRoot(int value, int x){
         //Find (value)^(1/x)
         //The answer will lie in the range [1 2 3 ... value]    Max: (value)^(1/1)
         double i = 1; double j = value;   //Find the value using binary search such that a*a*a*...x times = value
         double mid = 0;
-        while((j-i)>1e-6){  //Accurate upto 5 decimal points.
+        double precision = 1e-6;
+        while((j-i)>precision){  //Accurate upto 5 decimal points.
             mid = (i+j)/2;
 
-            if(mid*mid*mid<value)  i=mid;
+            if(power(mid, x)<value)  i=mid;
             else  j=mid;
         }
 
@@ -24,6 +33,6 @@ public:
 
 int main(){
     Solution newSol;
-    newSol.findRoot(26,3);
+    newSol.findRoot(15,4);
     return 0;
 }
