50243992808593a0bc0929edcb504127bea20497
RoOp Kumar Lala <roop.lala27@gmail.com>
2022-08-23 20:52:55 +0530

    Update Rough.cpp

--- Dynamic Programming/Rough.cpp
+++ Dynamic Programming/Rough.cpp
@@ -2,37 +2,71 @@
 using namespace std;
 
 class Solution {
+private:
+    bool f(int index, int target, vector<int> &array, vector<vector<int>> &dp){
+        if(target==0) return true;
+        if(index==0){
+            if(target == array[0]) return true;
+            else return false;
+        }
+
+        if(dp[index][target]!=-1) return dp[index][target];
+
+        int notTake = f(index-1, target, array, dp);
+        int take = false;
+        if(target>=array[index]) take = f(index-1, target-array[index], array, dp);
+
+        return dp[index][target] = (take||notTake);
+    }
+
 public:
-    int threeSumClosest(vector<int>& array, int target) {
-        sort(array.begin(), array.end());
-        int solution;
-        int n = array.size();
+    bool isPossible(vector<int> &array){
+        int sum=0;
+        for(const int &i: array) sum+=i;
         
-        int mini=1e8;
-        for(int iA=0; iA<=(n-3); iA++){
-            int sum=0;
-            int i=iA+1, j=n-1;
+        if(sum%2==0) sum=(sum/2);
+        else return false;
 
-            while(i<j){
-                // cout << array[iA] << " + " << array[i] << " + " << array[j] << endl;
-                sum = array[iA]+array[i]+array[j];
+        int li = array.size()-1;
+        vector<vector<int>> dp(li+1, vector<int>(sum+1, -1));
+        return f(li, sum, array, dp);
+    }
 
-                if(abs((target-sum))<mini){
-                    solution = sum;
-                    mini = abs(target-sum);
-                }
-                
-                if(target>sum) i++;
-                else    j--;
+    bool optimized(vector<int> &array){
+        if(array.size()<2) return false;
+
+        int sum=0;
+        for(const int &i: array) sum+=i;
+        
+        if(sum%2==0) sum=(sum/2);
+        else return false;
+
+        int li = array.size()-1;
+        vector<vector<int>> dp(li+1, vector<int>(sum+1, 0));    //DP[Index][Target]: Is it possible to achieve target in the array space of 0... Index
+        
+        //BaseCase:
+        for(int i=0; i<=li; i++) dp[i][0] = 1;
+        if(array[0]<=sum) dp[0][array[0]]=true;
+        
+        for(int index=1; index<=li; index++){
+            for(int target=1; target<=sum; target++){
+                int notTake = dp[index-1][target];
+                int take = false;
+                if(target>=array[index]) take = dp[index-1][target-array[index]];
+
+                dp[index][target] = (take||notTake);
+                cout << index << " " << target << " | " << dp[index][target] << endl;
             }
         }
-        
-        return solution;
+
+        return dp[li][sum];
     }
 };
 
 int main(){
-    vector<int> array = {1,1,1,0};
-    Solution newSol;
-    cout << newSol.threeSumClosest(array, -100);
+    vector<int> array = {9,5};
+    Solution find;
+    cout << find.optimized(array);
+
+    return 0;
 }
