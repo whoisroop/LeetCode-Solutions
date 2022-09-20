f92e94ce44c609825d4e19c7cfc4d8d91857c3b8
RavenClaaaw <81151656+RavenClaaaw@users.noreply.github.com>
2022-09-20 23:50:54 +0530

    Update Rough.cpp

--- Practice - DP/Subset/Rough.cpp
+++ Practice - DP/Subset/Rough.cpp
@@ -1,17 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Solution{
+public:
+    int findLCS(string sa, string sb){
+        int la = sa.size();
+        int lb = sb.size();
+        vector<vector<int>> dp(la+1, vector<int>(lb+1, -1));
+
+        //Tabulation & Index Shifting:
+        //BASE CASE: 
+        for(int i=0; i<=la; i++) dp[i][0] = 0;
+        for(int i=0; i<=lb; i++) dp[0][i] = 0;
+
+        for(int ia=1; ia<=la; ia++){
+            for(int ib=1; ib<=lb; ib++){
+                int longest = 0;
+                if(sa[ia-1] == sb[ib-1]) longest = 1 + dp[ia-1][ib-1];
+                else{
+                    longest = max( dp[ia-1][ib] , dp[ia][ib-1] );
+                }
+
+                dp[ia][ib] = longest;
+            }
+        }
+
+        return dp[la][lb];
+    }
+};
+
 int main(){
-    bitset<16> x;
-    x.set(1);
-    x.set(3);
-    x.set(11);
-    cout << x << endl;
-    bitset<16> y;
-    y.set(1);
-    y.set(2);
-    y.set(15);
-    cout << y << endl;
-    cout << (x^y);
+    string a = "12321";
+    string b = "32147";
+    Solution find;
+    cout << find.findLCS(a, b);
     return 0;
 }
